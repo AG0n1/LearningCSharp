c01fc075651c1aa6b052e7d732493e8b270c2da8
mathew markovets <mmarkovec15072003@gmail.com>
2024-04-09 16:26:03 +0300

    FirstLesson

--- FirstLesson/FirstLesson/Program.cs
+++ FirstLesson/FirstLesson/Program.cs
@@ -8,7 +8,7 @@ namespace FirstLesson
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Lesson1()
         {
             Console.WriteLine("Hello, World!");
             int some_mumber = 0;
@@ -19,16 +19,61 @@ namespace FirstLesson
                 {
                     Console.Write("1 ");
                 }
-                Console.WriteLine(");
+                Console.WriteLine("");
             }
             int value = 19;
             Console.WriteLine(someFunction(value));
             Console.WriteLine("Today I learned how to create my project, some of data-types and cycles. I outputed matrix 10x10");
         }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input lesson's number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case (1): Lesson1()
+                    break;
 
+                case (2): Console.WriteLine(2);
+                    break;
+                default: Console.WriteLine(3);
+                    break;
+            }
+            
+
+            Test test = new Test();
+            
+            Console.WriteLine(Test.test(60, "Math"));
+            Console.Read();
+        }
         static Type someFunction(int value)
         {
             return value.GetType();
         }
+    }
+
+    class Test
+    {
+        private int duration;
+        private string discipline;
+        public static string test(int duration, string discipline)
+        {
+            return $"This is {discipline} test with duration {duration} minutes";
+        }
+    }
+
+     class Interview : Test
+    {
+        
+    }
+
+    class Exam
+    {
+
+    }
+
+    class  FinalExam
+    {
+        
     }
 }
